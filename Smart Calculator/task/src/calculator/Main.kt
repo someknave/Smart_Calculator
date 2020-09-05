5f26378b8304c9b7deadea3a2ac68f16d1de9dd0
Joe Levey <josephfalevey@gmail.com>
2020-09-06 03:35:01 +1000

    Stage 1 Complete

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,5 +1,20 @@
 package calculator
 
+import java.util.*
+
 fun main() {
-    println("Hello, World!")
+    val scanner = Scanner(System.`in`)
+    val sum = scanner.nextLine().lineToIntList().listAdd()
+
+    println(sum)
+}
+
+
+
+fun String.lineToIntList(): List<Int> {
+    return this.split(" ").map { it.toInt() }
+}
+
+fun List<Int>.listAdd(): Int {
+    return this.fold(0) { acc, it -> acc + it }
 }
